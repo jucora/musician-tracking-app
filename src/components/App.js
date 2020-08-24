c9464b08f75c4fc1be305634178ba0eadff1e08b
jucora <artjulius@hotmail.com>
2020-08-24 01:59:14 -0500

    Add react-router-dom and basic routes to Home page and Dashboard page

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './Home';
+import Dashboard from './Dashboard';
 
-function App() {
-  return <div className="App">test</div>;
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/dashboard" component={Dashboard} />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
