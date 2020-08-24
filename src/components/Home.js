19152d1650f8cbf7f9d51800c84c8160dcbd0664
jucora <artjulius@hotmail.com>
2020-08-24 11:48:01 -0500

    Redirect user to dashboard once the account is created

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,11 +2,20 @@ import React from 'react';
 import Registration from './auth/Registration';
 
 export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleSuccessfulAuth = this.handleSuccessfulAuth.bind(this);
+  }
+
+  handleSuccessfulAuth(data) {
+    //this.props.handleLogin(data);
+    this.props.history.push('/dashboard');
+  }
   render() {
     return (
       <div>
         <h1>Home Page</h1>
-        <Registration />
+        <Registration handleSuccessfulAuth={this.handleSuccessfulAuth} />
       </div>
     );
   }
