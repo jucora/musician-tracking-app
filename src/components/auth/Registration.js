68f89c293e138b2c938fb841035db771d6563851
jucora <artjulius@hotmail.com>
2020-08-24 11:15:52 -0500

    Add auth folder with Registration component to allow users to create new accounts

---
+++ src/components/auth/Registration.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import axios from 'axios';
+
+export default class Registration extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      password_confirmation: '',
+      registrationErrors: '',
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleSubmit(e) {
+    const { email, password, password_confirmation } = this.state;
+
+    e.preventDefault();
+    axios
+      .post(
+        'http://localhost:3001/registrations',
+        {
+          user: {
+            email: email,
+            password: password,
+            password_confirmation: password_confirmation,
+          },
+        },
+        { withCredentials: true }
+      )
+      .then((response) => {
+        this.props.handleSuccessfulAuth(response.data);
+      })
+      .catch((error) => {
+        console.log('Registration error', error);
+      });
+  }
+
+  handleChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="email"
+            name="email"
+            placeholder="Your Email"
+            value={this.state.email}
+            onChange={this.handleChange}
+            required
+          />
+
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={this.state.password}
+            onChange={this.handleChange}
+            required
+          />
+
+          <input
+            type="password"
+            name="password_confirmation"
+            placeholder="Password confirmation"
+            value={this.state.password_confirmation}
+            onChange={this.handleChange}
+            required
+          />
+
+          <button type="submit">Register</button>
+        </form>
+      </div>
+    );
+  }
+}
