568142504c12858f19a5f33ef203fc7eb2b192f4
jucora <artjulius@hotmail.com>
2020-08-27 17:36:10 -0500

    Add custom chart to display user progress

--- src/components/Progress.js
+++ src/components/Progress.js
@@ -1,7 +1,64 @@
 import React from 'react';
+import { Line } from 'react-chartjs-2';
+import axios from 'axios';
 
-const Progress = () => {
-  return <div>Progress here</div>;
-};
+class Progress extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {
+        labels: [],
+        datasets: [
+          {
+            label: 'Progress',
+            backgroundColor: 'rgba(0, 255, 0, 0.75)',
+            data: null,
+          },
+        ],
+      },
+    };
+  }
+  componentDidMount() {
+    axios
+      .get('http://localhost:3001/skills', { withCredentials: true })
+      .then((response) => {
+        let skills = [];
+        let values = [];
+
+        response.data.currentSkills.map((skill) => {
+          skills.push(skill.name);
+          values.push(skill.sum);
+        });
+
+        console.log('values are', values);
+        this.setState({
+          data: { labels: skills },
+          data: { datasets: [{ label: 'Your Progress', data: values }] },
+        });
+      })
+      .catch((error) => {
+        console.error('error', error);
+      });
+  }
+  render() {
+    return (
+      <div
+        style={{
+          position: 'relative',
+          width: '60%',
+          height: 'auto',
+          margin: 'auto',
+        }}
+      >
+        <Line
+          options={{
+            responsive: true,
+          }}
+          data={this.state.data}
+        />
+      </div>
+    );
+  }
+}
 
 export default Progress;
