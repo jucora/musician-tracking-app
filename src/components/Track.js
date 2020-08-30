c061a14e6f9bb9cea255e37b3691396a3726200d
jucora <artjulius@hotmail.com>
2020-08-30 00:58:31 -0500

    Add total score in Track component

--- src/components/Track.js
+++ src/components/Track.js
@@ -3,18 +3,46 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Skill from './Skill';
+import axios from 'axios';
 
-const Track = (props) => {
-  const { user, loggedInStatus, history } = props;
-  return loggedInStatus === 'LOGGED_IN' ? (
-    <div className="track">
-      <h2>Hello {user.email}</h2>
-      <Skill />
-    </div>
-  ) : (
-    (history.push('/'), true)
-  );
-};
+class Track extends React.Component {
+  constructor() {
+    super();
+    this.state = { total: 0, month: 'Select a month', today: 0 };
+  }
+  componentDidMount() {
+    axios
+      .get('http://localhost:3001/measures', { withCredentials: true })
+      .then((response) => {
+        console.warn('resul', response);
+        this.setState({
+          total: response.data.totalScore[0].total,
+        });
+      })
+      .catch((error) => {
+        console.error('error', error);
+      });
+  }
+
+  render() {
+    const { user, loggedInStatus, history } = this.props;
+    const { total } = this.state;
+    return loggedInStatus === 'LOGGED_IN' ? (
+      <div className="track">
+        <h2>Hello {user.email}</h2>
+        <div className="records">
+          <div>
+            <h2>Total Hours</h2>
+            <p>{total}</p>
+          </div>
+        </div>
+        <Skill />
+      </div>
+    ) : (
+      (history.push('/'), true)
+    );
+  }
+}
 
 Track.propTypes = {
   user: PropTypes.objectOf(PropTypes.any).isRequired,
